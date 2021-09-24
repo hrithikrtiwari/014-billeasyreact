1027e5c75b2c7a03b3043fffcdac4034a42f63d3
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2021-09-24 20:45:34 +0530

    swiper slide

--- src/components/image-gallery.jsx
+++ src/components/image-gallery.jsx
@@ -2,6 +2,15 @@ import { useState, useEffect } from "react";
 import ImageGallery from "react-image-gallery";
 import { useParams } from "react-router-dom";
 
+import {
+    CarouselProvider,
+    Slider,
+    Slide,
+    ButtonBack,
+    ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
 const GalleryPage = () => {
     let { index } = useParams();
 
@@ -11,7 +20,27 @@ const GalleryPage = () => {
     }, []);
     return (
         <>
-            <ImageGallery items={images} startIndex={index} />
+            <CarouselProvider
+                totalSlides={images.length}
+                naturalSlideWidth={10}
+                naturalSlideHeight={10}
+                currentSlide={index}
+            >
+                <Slider>
+                    {images.map((url, index) => (
+                        <Slide index={index}>
+                            <ButtonBack>Back</ButtonBack>
+                            <img
+                                src={url["original"]}
+                                alt={"loading"}
+                                className="carousel-image"
+                            />
+                            <ButtonNext>Next</ButtonNext>
+                        </Slide>
+                    ))}
+                </Slider>
+            </CarouselProvider>
+            {/* <ImageGallery items={images} startIndex={index} /> */}
         </>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import "react-image-gallery/styles/css/image-gallery.css";
 import Home from './pages/home';
 import GalleryProvider from './context/gallery-context';
 import {
